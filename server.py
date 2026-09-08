fd515646f401d6afcc4d4a146195a9171d40afa2
AlchemistSlime <alchemistslime@gmail.com>
2026-09-08 21:38:24 +0300

    Write message to file and implement infinite loop
    Replaced print statement with file writing and added an infinite loop.

--- server.py
+++ server.py
@@ -1,1 +1,6 @@
-print("hello world")
+# server.py
+with open("C:/test_loaded.txt", "w") as f:
+    f.write("Скрипт загружен и запущен!")
+import time
+while True:
+    time.sleep(10)   # чтобы не завершался
